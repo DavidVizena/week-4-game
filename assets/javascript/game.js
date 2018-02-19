5c4652d86e0dfdb7ac8491e8088914802ecef3d1
Vizena2005 <vizena2005@gmail.com>
2018-02-19 00:10:13 -0600

    adding variable and finishing the html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,48 @@
 $(document).ready(function(){
-
-
-
-
-
-
-
+// Global Variables
+var user = {};
+var opponents = {};
+var harryPotter = {
+        Name: "Harry Potter",
+        Attack: function (){
+           x += 6;},
+        Health: 100, 
+        CounterAttack: 6
+};
+    var hermione = {
+        Name: "Hermione Granger",
+        Attack: function (){
+            x += 4;},
+        Health: 120,
+        CouterAttack: 4
+};
+    var ron = {
+        Name: "Ron Weasley",
+        Attack: function (){
+            x += 5;},
+        Health: 110,
+        CounterAttack: 5
+};
+    var draco = {
+        Name: "Draco Malfoy",
+        Attack: function (){
+            x += 3;},
+        Health: 140,
+        CounterAttack: 3
+};
     
+
+$(".harryPotter").on("click", function() {
+    $(".harryPotter").hide();
 });
+
+$(".hermione").on("click", function(){
+    $(".hermione").hide();
+});
+
+
+$(".harryPotter").html(harryPotter.Health);
+});
+
+
+
